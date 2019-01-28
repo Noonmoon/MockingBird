89e3e79a500456812162de5f45a2d605d9d3fa6a
Noonmoon <tristandeering@hotmail.com>
2019-01-27 17:01:46 -0700

    input validation notifications

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,11 @@ router.get('/register', function(req, res, next) {
 router.post('/register', function(req, res, next) {
 
   req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-  req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+  req.checkBody('username', 'Username must be between 3-15 characters long.').len(3, 15);
   req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
   req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-  req.checkBody('password', 'Password must be between 8-60 characters long.').len(8, 60);
-  req.checkBody("password", "Password must include one lowercase character, one uppercase character, and a number.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-  req.checkBody('passwordMatch', 'Password must be between 8-60 characters long.').len(8, 60);
+  req.checkBody('password', 'Password must be between 8-30 characters long.').len(8, 30);
+  req.checkBody("password", "Password must include one lowercase character, one uppercase character, and a number.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30}$/);
   req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
   const errors = req.validationErrors();
@@ -30,20 +29,25 @@ router.post('/register', function(req, res, next) {
   if (errors) {
     console.log(`errors: ${JSON.stringify(errors)}`);
 
-    res.render('register', {title: 'Registration Error'});
+    res.render('register', {
+      title: 'Registration Error',
+      errors: errors
+    })
+
+  } else {
+
+    let username = req.body.username;
+    let password = req.body.password
+    let email = req.body.email
+
+    const db = require('../db.js');
+
+    db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], function(error, results, fields) {
+      if (error) throw error;
+
+      res.render('index', {title: 'Registration Complete'});
+    })
   }
-
-  let username = req.body.username;
-  let password = req.body.password
-  let email = req.body.email
-
-  const db = require('../db.js');
-
-  db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password], function(error, results, fields) {
-    if (error) throw error;
-
-    res.render('index', {title: 'Registration Complete'});
-  })
 });
 
 // route for user Login
