5f0a82b7eea0adbae40d616ecec0f31a5c6c1914
Noonmoon <tristandeering@hotmail.com>
2019-02-26 21:01:07 -0700

    newsfeed functionality complete

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,47 @@
+// used to prevent xss
+var escape = function(unsafe) {
+  return unsafe
+   .replace(/&/g, "&amp;")
+   .replace(/</g, "&lt;")
+   .replace(/>/g, "&gt;")
+   .replace(/"/g, "&quot;")
+   .replace(/'/g, "&#039;");
+ };
+
 if (auth) {
+  function createPostElement(post, escape) {
+    var $post =
+      `<div>
+        <a class="btn postcard" href='#' role='button'>
+          <img class="rounded-circle" src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==' width='100' height='100'>
+          <h2>${post.user_id}</h2>
+          <p>
+            ${escape(post.text)}
+          </p>
+        </a>
+      </div>`;
+    return $post;
+  }
+  console.log(auth)
+
   $(function () {
+
+    // forms new post
+    function renderPosts(posts) {
+      posts.forEach(function(post) {
+        var $post = createPostElement(post, escape);
+        $('#newsfeed').prepend($post);
+      });
+    }
+
+    // grabs and renders all current tweets
+    function grabPosts() {
+      $.get(`/post/noonmoon`, function(data) {
+        renderPosts(data);
+      });
+    }
+    grabPosts();
+
 
   });
 }
